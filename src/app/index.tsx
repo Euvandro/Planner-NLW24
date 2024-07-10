971749ce0fb032f39aee5c32fff5a8e0840ba7d5
EVANDRO MACHADO PEREIRA <evandromachadopereira@hotmail.com>
2024-07-10 17:01:16 -0300

    Aula 1 - Button 2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,38 +1,61 @@
-import {View, Text, Image} from 'react-native';
-import { MapPin, Calendar as IconCalendar } from 'lucide-react-native';
+import { View, Text, Image } from 'react-native';
+import { MapPin, Calendar as IconCalendar, Settings2, UserRoundPlus, ArrowRight } from 'lucide-react-native';
 import { Input } from '@/components/input';
 import { colors } from '@/styles/colors';
 import { Button } from '@/components/button';
 
 
-export default function Index(){
+enum StepForm{
+    
+}
+export default function Index() {
 
+    function handleNextStepForm(){
+
+    }
 
     return (
         <View className='flex-1 justify-center items-center px-5'>
 
 
-            <Image source={require("@/assets/logo.png")} className='h-8' resizeMode='contain'/>
+            <Image source={require("@/assets/logo.png")} className='h-8' resizeMode='contain' />
             <Text className='text-zinc-400 font-regular text-center text-lg mt-3'>Convide seus amigos e planeje sua{"\n"}próxima viagem</Text>
 
 
             <View className='w-full bg-zinc-900 p-4 rounded-xl my-8 border'>
                 <Input>
-                    <MapPin color={colors.zinc[400]} size={20}/>
-                    <Input.Field placeholder='Para onde?'/>
+                    <MapPin color={colors.zinc[400]} size={20} />
+                    <Input.Field placeholder='Para onde?' />
                 </Input>
 
                 <Input>
-                    <IconCalendar color={colors.zinc[400]} size={20}/>
-                    <Input.Field placeholder='Para onde?'/>
+                    <IconCalendar color={colors.zinc[400]} size={20} />
+                    <Input.Field placeholder='Quando?' />
                 </Input>
 
                 <View className='border-b py-3 border-zinc-800 '>
-                    <Button>
+                    <Button variant='secondary'>
                         <Button.Title>Alterar local/data</Button.Title>
+                        <Settings2 color={colors.zinc[200]} size={20} />
                     </Button>
                 </View>
+
+                <Input>
+                    <UserRoundPlus color={colors.zinc[400]} size={20} />
+                    <Input.Field placeholder='Quem estará na viagem?' />
+                </Input>
+
+                <Button onPress={handleNextStepForm}>
+                    <Button.Title>Continuar</Button.Title>
+                    <ArrowRight color={colors.lime[950]} size={20} />
+                </Button>
+
             </View>
+
+            <Text className='text-zinc-500 font-regular text-center text-base'>
+                Ao planejar sua viagem pela plann.er você automaticamente concorda com nossos{" "}
+                <Text className='text-zinc-300 underline'>termos de uso e políticas de privacidade.</Text>
+            </Text>
 
 
         </View>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,5 @@
-import { TouchableOpacity, Text, TouchableOpacityProps, TextProps } from "react-native";
+import { createContext, useContext } from "react";
+import { TouchableOpacity, Text, TouchableOpacityProps, TextProps, View, ActivityIndicator } from "react-native";
 
 import clsx from "clsx";
 
@@ -9,16 +10,39 @@ type ButtonProps = TouchableOpacityProps & {
     isLoading?: boolean
 }
 
+const ThemeContext = createContext<{ variant?: Variants }>({})
+
 function Button({ variant = "primary", isLoading, children, ...rest }: ButtonProps) {
-    return(
-    <TouchableOpacity className="w-full bg-lime-500">
-        {children}
-    </TouchableOpacity>
+    return (
+        <TouchableOpacity
+            activeOpacity={0.7}
+            disabled={isLoading}
+            {...rest}
+        >
+            <View className={clsx(
+                "h-11 flex-row items-center justify-center rounded-lg gap-2 px-2",
+                {
+                    "bg-lime-300": variant === "primary",
+                    "bg-zinc-800": variant === "secondary",
+                },
+            )}
+            >
+                <ThemeContext.Provider value={{ variant }}>
+                    {isLoading ? <ActivityIndicator className="text-lime-950"/> : children}
+                </ThemeContext.Provider>
+            </View>
+
+        </TouchableOpacity>
     )
 }
 
 function Title({ children }: TextProps) {
-    return <Text>{children}</Text>
+
+    const { variant } = useContext(ThemeContext)
+
+    return <Text className={clsx("text-base font-semibold", {"text-lime-950": variant === 'primary', "text-zinc-200": variant === 'secondary'})}>
+        {children}
+    </Text>
 }
 
 Button.Title = Title;
